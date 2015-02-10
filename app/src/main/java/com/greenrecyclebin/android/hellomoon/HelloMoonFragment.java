141a99ebbcf172de59202133d244a50d4c980d7f
Daniel Le <greenrecyclebin@gmail.com>
2015-02-10 12:56:10 +0800

    Add video player

--- app/src/main/java/com/greenrecyclebin/android/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/com/greenrecyclebin/android/hellomoon/HelloMoonFragment.java
@@ -1,5 +1,6 @@
 package com.greenrecyclebin.android.hellomoon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.VideoView;
 
 /**
  * Created by greenrecyclebin on 8/2/15.
@@ -21,12 +23,17 @@ public class HelloMoonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
+        final VideoView videoView = (VideoView) v.findViewById(R.id.hellomoon_videoView);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.apollo_17_stroll));
+
         playButton = (Button) v.findViewById(R.id.hellomoon_playButton);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 player.play(getActivity());
+
+                videoView.start();
 
                 playButton.setEnabled(false);
                 pauseButton.setEnabled(true);
@@ -42,6 +49,8 @@ public class HelloMoonFragment extends Fragment {
             public void onClick(View v) {
                 player.pause();
 
+                videoView.pause();
+
                 playButton.setEnabled(true);
                 pauseButton.setEnabled(false);
                 stopButton.setEnabled(true);
@@ -55,6 +64,9 @@ public class HelloMoonFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 player.stop();
+
+                videoView.stopPlayback();
+                videoView.resume();
 
                 playButton.setEnabled(true);
                 pauseButton.setEnabled(false);
